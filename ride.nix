977724842af52d69944e40933287be976961c717
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-06-04 20:27:37 +0200

    Add and update flake inputs, refactor RIDE derivation

--- ride.nix
+++ ride.nix
@@ -1,4 +1,6 @@
-{ lib
+{ src
+, rev
+, lib
 , fetchFromGitHub
 , buildNpmPackage
 , makeWrapper
@@ -8,41 +10,37 @@
 
 let
   pname = "ride";
-  version = "4.5.3770";
-  rev = "246649aad81daadfa8ac7e0af7c0206e49b4e337";
 
-  src = fetchFromGitHub {
-    inherit rev;
-    owner = "Dyalog";
-    repo = pname;
-    hash = "sha256-j3flZ1sKsYRkJ79yxmR4NKcOSf66bHsCXiupKhUAFcQ=";
-  };
+  packageInfo = builtins.fromJSON (builtins.readFile (src + "/package.json"));
+
+  version = lib.concatStringsSep "." (lib.take 2 (lib.splitString "." packageInfo.version));
 
   versionJSON = builtins.toJSON {
     versionInfo = {
       inherit version rev;
-      date = "2023-05-17 17:45:47 +0200";
+      date = "unknown (built by Nix)";
     };
   };
+
 in
 buildNpmPackage {
 
   inherit pname version src;
 
-  # Skips the auto-downloaded electron-chromedriver binary
-  postPatch = "sed -i '/spectron/d' package.json";
+  npmInstallFlags = [ "--omit=dev" ];
 
   # Skips the auto-downloaded electron binary
   ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
-  npmDepsHash = "sha256-qA+Hzz5dvxLEpPVPCvfngUhfqm897lKQt0DYWDk9PoI=";
+  npmDepsHash = "sha256-mgkOTuspqoM4yZMr2u7f+0qSgzIMz033GXezuPA7rkQ=";
+
   dontNpmBuild = true;
 
   nativeBuildInputs = [ makeWrapper python3 ];
 
   # This is the replacement for the `mk` script in the source repo
   postInstall = ''
-    cd $out/lib/node_modules/ride45
+    cd $out/lib/node_modules/${packageInfo.name}
 
     mkdir $out/app
     cp -r {src,lib,node_modules,D.png,favicon.*,*.html,main.js,package.json} $out/app
@@ -50,13 +48,18 @@ buildNpmPackage {
     mkdir $out/app/style
     cp -r style/{fonts,img,*.css} $out/app/style
 
+    cd $out/app/node_modules
+    rm -r {.bin,monaco-editor/{dev,esm,min-maps}}
+    find . -type f -name '*.map' -exec rm -rf {} +
+    find . -type d -name 'test' -exec rm -rf {} +
+
     rm -r $out/lib
 
     # Generate version-info
     mkdir $out/app/_
     echo 'D=${versionJSON}' > $out/app/_/version.js
     echo ${version} > $out/app/_/version
-  
+
     # Call electron manually
     makeWrapper ${electron}/bin/electron $out/bin/ride \
             --add-flags $out/app
